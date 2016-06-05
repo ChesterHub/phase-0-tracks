13869b2b38ccd4205558a16bcf2ea97cf574b80a
Chester Hoang <chesterlhoang@gmail.com>
2016-06-04 23:39:19 -0700

    53 branch

---
+++ ruby/yes/test.rb
@@ -0,0 +1,17 @@
+applicant = {
+  name: "Anishka Clarke", 
+  address: "1451 Broadway Avenue, New York, NY", 
+  email: "anishka@interiordesigngeniuses.com", 
+  phone: nil, 
+  fave_blue: "indigo", 
+  likes_paisley: true, 
+  likes_chevrons: false, 
+  likes_photorealistic_woods: false, 
+  likes_abstract_woods: true, 
+  ombre: "Practically medieval in its appalling irrelevance"
+}
+
+applicant[:name] = "silas"
+
+puts applicant
+
