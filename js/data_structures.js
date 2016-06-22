a05624f10094aa113b0fc8cc14cd4d6d2c77371e
Chester Hoang <ChesterLHoang@gmail.com>
2016-06-21 22:56:04 -0700

    final

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,21 @@ for (var c = 0; c < colors.length; c++) {
 
 console.log(horses);
 
+function Car(make, model, year) {
+	"New Car:", this;
+	this.make = make;
+	this.model = model;
+	this.year = year;
+
+	this.gas = function() { console.log("VROOOOOOOOM!");};
+}
+
+var car1 = new Car("Acura","NSX","2017");
+console.log(car1)
+car1.gas();
+
+var car2 = new Car("Infiniti","Q60 Red Sport","2017");
+car2.gas();
+
+var car3 = new Car("BMW","M4","2016");
+console.log(car3);
