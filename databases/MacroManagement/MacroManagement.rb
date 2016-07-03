07bbc6dc8d36bb6f17528bae5140f815ee6d336c
Chester Hoang <ChesterLHoang@gmail.com>
2016-07-02 23:33:33 -0700

    add the ruby code to insert data and then return the results

--- databases/MacroManagement/MacroManagement.rb
+++ databases/MacroManagement/MacroManagement.rb
@@ -1,32 +1,36 @@
 require 'sqlite3'
-require 'faker'
 
-database = sqlite3::Database.new("MacroDatabase.db")
+db = SQLite3::Database.new("bodystats.db")
+#db.results_as_hash = true
 
-
-body_table = <<-SQL
-CREATE TABLE IF NOT EXISTS Body_Stats(
-id INTEGER PRIMARY KEY,
-BodyFatPercentage INT,
-Weight INT,
-Date_Entered datetime NOT NULL DEFAULT CURDATE()
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS bodystats (
+    id INTEGER PRIMARY KEY,
+    fatpercentage VARCHAR(255),
+    weight VARCHAR(255),
+    date_entered datetime NOT NULL DEFAULT CURRENT_DATE
   )
 SQL
 
-database.execute(body_table)
+db.execute(create_table_cmd)
 
-results = database.execute("SELECT * FROM Body_Stats")
-p results
-
-
-until enterdata == "n" do
+enterdata = ""
 puts "Would you like to enter a new date of body stats? Y/N"
-enterdata = gets.chomp.lowercase
-puts "What is your current bodyfat percentage? Do not enter the % key."
-bodyfat = gets.chomp.lowercase
-puts "What is your current weight?"
-weight = gets.chomp.lowercase
-database.execute("INSERT INTO Body_Stats (BodyFatPercentage, Weight) VALUES (?,?)", [BodyFatPercentage, Weight])
+enterdata = gets.chomp.downcase
+until enterdata == "n" do
+	puts "What is your current bodyfat percentage? Do not enter the % key."
+	bodyfat = gets.chomp.downcase
+	puts "What is your current weight?"
+	weight = gets.chomp.downcase
+	db.execute("INSERT INTO bodystats (fatpercentage, weight) VALUES (?,?)", [bodyfat, weight])
+	puts "Would you like to enter another date of body stats? Y/N"
+	enterdata = gets.chomp.downcase
+end
+
+results = db.execute("SELECT * FROM bodystats")
+
+results.each do |row|
+puts "On #{row[3]} Chester weighed #{row[2]} pounds and had a bodyfat of #{row[1]}%."
 end
 
 
